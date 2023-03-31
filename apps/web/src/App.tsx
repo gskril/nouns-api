225bd7fd409add353989266d2312df057f2e6829
Greg <gskril@gmail.com>
2023-03-31 00:55:17 -0400

    add web app

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <div />
+
+      <main>
+        <div className="hero">
+          <h1 className="title">GraphQL API to power your Nounish ideas</h1>
+          <div className="hero__buttons">
+            <a className="btn" href="https://nouns-data.up.railway.app/graphql">
+              Try it now
+            </a>
+            <a
+              className="btn btn--secondary"
+              href="https://nouns.gregskril.com/"
+            >
+              See it in action
+            </a>
+          </div>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <span>
+          by <a href="https://twitter.com/gregskril/">@gregskril</a>
+        </span>
+        <span>
+          code on <a href="https://github.com/gskril/nouns-api">github</a>
+        </span>
+      </footer>
+    </div>
+  )
+}
+
+export default App
